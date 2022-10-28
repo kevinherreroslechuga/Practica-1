729e2725aad62241241115af160fdb79ad6cea88
kevinherreroslechuga <113234810+kevinherreroslechuga@users.noreply.github.com>
2022-10-28 14:49:23 -0500

    carga

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,23 @@
-const baseEndpoint = 'https://api.github.com';
+const baseEndpoint = 'https://api.github.com';// en esta funcion extraemos la informacion del json e imprimimos name, blog, locacion del usuario se imprime la informacion del json 
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
+const $n = document.querySelector('.name');//agrege el . por que referencia una clase
+const $b = document.querySelector('.blog');
 const $l = document.querySelector('.location');
 
-function displayUser(username) {
+async function displayUser(username) { //agrege async a la funcion 
   $n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`);
+  const response = await fetch(`${usersEndpoint}/${username}`);//cambia la funcion a asincrona para que permitiera el await
+  const data= await response.json();// declaramos la variable de data con await
   console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  $n.textContent = `${data.name}`;//corregi las comillas de estas declaraciones de texto
+  $b.textContent = `${data.blog}`;
+  $l.textContent = `${data.location}` ;
 }
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  $n.textContent = `Algo salió mal: ${err}`;//le faltaba el simbolo "$" al nombre d el avariable
 }
 
 displayUser('stolinski').catch(handleError);
